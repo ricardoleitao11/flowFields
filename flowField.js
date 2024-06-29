08a18527459ab0cd9035f638021a3107effff9b4
Ricardo Leitao <ricardogleitao@hotmail.com>
2024-06-29 01:21:47 -0300

    Second fase of the project. Here, we created a noise-based randomized blur, painting the pixels in a gray scale. In this fase, it is applied the Perlin Noise over two dimensions.

--- flowField.js
+++ flowField.js
@@ -1,51 +1,28 @@
-//var xoff1 = 0;
-//var xoff2 = 10000;
-
 var inc = 0.01;
-var start = 0;
-
 function setup() {
   createCanvas(400, 400);
 }
 
+
+
 function draw() {
-  background(51);
-  stroke(255);
-  noFill();
-  beginShape();
-  var xoff = start;
-  
-  for (var x = 0; x < width; x++){
-    stroke(255);
+  var yoff = 0;
+  noiseDetail(28);
+  loadPixels();
+  for (var y = 0; y < height; y++){
+    var xoff = 0;
+    for (var x = 0; x < width; x++){
+      var index = (x + y * width) * 4;
+      var r = noise(xoff, yoff) * 255;
+      pixels[index+0] = r;
+      pixels[index+1] = r;
+      pixels[index+2] = r;
+      pixels[index+3] = 255;
+      xoff += inc;
+    }
+    yoff += inc;
     
-    
-    // SINE WAVE MIXED WITH PERLIN NOISE 
-    // var n = map(noise(xoff), 0, 1, -50, 50);
-    // var s = map(sin(xoff), -1, 1, -0, height);
-    // var y = n + s;
-    
-    
-    
-    var y = noise(xoff) * height;
-    vertex(x, y);
-    
-    xoff += inc;
   }
-  endShape();
-  
-  start += inc;
-  
+  updatePixels();
   //noLoop();
-  
-  
-  
-  // GENERIC RANDOMIZED BALL POSITION
-  //var x = map(noise(xoff1), 0, 1, 0, width);
-  //var y = map(noise(xoff2), 0, 1, 0, height);
-  //var t = map(noise(100), 0, 1, 20, 50)
-  //xoff1 += 0.005;
-  //xoff2 += 0.008;
-  
-  //ellipse(x, y, 24, 24);
-  //ellipse(x, y, t);
 }
